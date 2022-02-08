4fca47de3aefad56c4c87ae8f9c08c8f42fbd2d3
handsomeshalaby <97477949+handsomeshalaby@users.noreply.github.com>
2022-02-08 20:31:25 +0200

    create proudct, wishlist, catalog screens

---
+++ lib/screens/wishlist/wishlist_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:githubexample/widgets/widgets.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(title: 'home page'),
+      bottomNavigationBar: CustomNavBar(),
+    );
+  }
+}
