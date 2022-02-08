ae3a8d3b5c98197d33d5b3c4eaf7ed139b4440fb
handsomeshalaby <97477949+handsomeshalaby@users.noreply.github.com>
2022-02-09 01:08:47 +0200

    create product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,54 @@
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable {
+  final String name;
+  final String category;
+  final String imageUrl;
+  final double price;
+  final bool isRecommended;
+  final bool isPopular;
+  const Product(
+      {required this.name,
+      required this.category,
+      required this.imageUrl,
+      required this.price,
+      required this.isRecommended,
+      required this.isPopular});
+  @override
+  List<Object?> get props =>
+      [name, category, imageUrl, price, isRecommended, isPopular];
+  static List<Product> products = [
+    Product(
+        name: 'soft drink #1',
+        category: 'soft drinks',
+        imageUrl:
+            'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHN8ZW58MHx8MHx8&w=1000&q=80',
+        price: 299.0,
+        isRecommended: true,
+        isPopular: false),
+    Product(
+        name: 'soft drink #2',
+        category: 'soft drinks',
+        imageUrl:
+            'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHN8ZW58MHx8MHx8&w=1000&q=80',
+        price: 299.0,
+        isRecommended: false,
+        isPopular: true),
+    Product(
+        name: 'soft drink #3',
+        category: 'soft drinks',
+        imageUrl:
+            'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHN8ZW58MHx8MHx8&w=1000&q=80',
+        price: 299.0,
+        isRecommended: true,
+        isPopular: false),
+    Product(
+        name: 'soft drink #4',
+        category: 'soft drinks',
+        imageUrl:
+            'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHN8ZW58MHx8MHx8&w=1000&q=80',
+        price: 299.0,
+        isRecommended: false,
+        isPopular: true),
+  ];
+}
