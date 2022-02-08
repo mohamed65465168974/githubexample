f78aa8949d0e572a003d267b23099ca0081d1d0b
handsomeshalaby <97477949+handsomeshalaby@users.noreply.github.com>
2022-02-08 21:36:48 +0200

    create cateogry model class

---
+++ lib/models/category_model.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+class Category extends Equatable {
+  final String name;
+  final String imageUrl;
+  const Category({required this.name, required this.imageUrl});
+  @override
+  List<Object?> get props => [name, imageUrl];
+
+  static List<Category> categories = [
+    Category(
+        name: 'soft drink',
+        imageUrl:
+            'https://www.thespruceeats.com/thmb/LHQcA-aMyrKhTsFhfmHxqkgz6CI=/4000x2666/filters:fill(auto,1)/greyhound-cocktail-recipe-759306-hero-01-680428b9f8d24389a2b547a52f3c279d.jpg'),
+    Category(
+        name: 'soft drink',
+        imageUrl:
+            'https://info.ehl.edu/hubfs/Blog-EHL-Insights/Blog-Header-EHL-Insights/trends%20drinks.jpg'),
+    Category(
+        name: 'soft drink',
+        imageUrl:
+            'https://static01.nyt.com/images/2019/05/15/dining/09spritz1/merlin_141351063_e6829ad6-f61c-435a-b6c3-87526a0613ef-superJumbo.jpg'),
+  ];
+}
