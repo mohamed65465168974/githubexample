0f56f0a70f4314e86c5256770be9a74ab1d151f0
handsomeshalaby <97477949+handsomeshalaby@users.noreply.github.com>
2022-02-09 21:38:49 +0200

    create wishlist bloc

---
+++ lib/blocs/wishlist_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+import 'package:bloc/bloc.dart';
+part 'wishlist_event.dart';
+part 'wishlist_state.dart';
+
+class WishlistBloc extends Bloc<WishlistEvent, WishlistState> {
+  WishlistBloc() : super(WishlistInitial()) {
+    on<WishlistEvent>((event, emit) {});
+  }
+}
