d6c09a3690ae7ddc78fe73d188bcaf75f811091f
Mohamed Shalaby <mohamedshalaby0097@gmail.com>
2022-02-11 08:09:47 +0200

    create card product card in cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:githubexample/screens/screens.dart';
 import 'package:githubexample/widgets/widgets.dart';
+import '../../models/models.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -9,6 +12,38 @@ class CartScreen extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(title: 'cart page'),
       bottomNavigationBar: CustomNavBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'add \$20.0 for FREE Delivery',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    elevation: 0.0,
+                  ),
+                  onPressed: () {
+                    Get.offAll(HomeScreen());
+                  },
+                  child: Text(
+                    'Add more items',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            CardProductCard(product: Product.products[0]),
+          ],
+        ),
+      ),
     );
   }
 }
