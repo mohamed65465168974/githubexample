c1a7479b35b969dc2ecc047bc7a95bb2ba14f7ca
Mohamed Shalaby <mohamedshalaby0097@gmail.com>
2022-02-11 08:25:34 +0200

    add card product to cart screen

---
+++ lib/widgets/cart_product_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+
+class CardProductCard extends StatelessWidget {
+  final Product product;
+  const CardProductCard({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        children: [
+          Image.network(
+            product.imageUrl,
+            width: 100,
+            height: 80,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.name,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text(
+                  '${product.price} EGP',
+                  style: TextStyle(color: Colors.green),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.remove_circle),
+              ),
+              Text(
+                '1',
+                style: TextStyle(fontSize: 15),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add_circle),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
