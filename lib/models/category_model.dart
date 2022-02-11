5b883d868fb3daff2e5f2f5a4c42d566c656f304
Mohamed Shalaby <mohamedshalaby0097@gmail.com>
2022-02-12 00:02:51 +0200

    convert data from firebase to instances of models

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
 class Category extends Equatable {
@@ -6,6 +7,12 @@ class Category extends Equatable {
   const Category({required this.name, required this.imageUrl});
   @override
   List<Object?> get props => [name, imageUrl];
+
+  static Category fromSnapshot(DocumentSnapshot snapshot) {
+    Category category =
+        Category(name: snapshot['name'], imageUrl: snapshot['imageUrl']);
+    return category;
+  }
 
   static List<Category> categories = [
     Category(
